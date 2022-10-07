1226744765ad7e1f7a8280266f3243eef7d5efba
notpankaj <iampankaj0409@gmail.com>
2022-10-07 22:09:53 +0530

    car , register , login page created

--- server.js
+++ server.js
@@ -5,14 +5,17 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-// Assets
-app.use(express.static("public"));
-app.get("/", (req, res) => res.render("home"));
-
 // set Template engine
 app.use(expressLayout);
 app.set("views", path.join(__dirname, "/resources/views"));
 app.set("view engine", "ejs");
+
+// Assets
+app.use(express.static("public"));
+app.get("/", (_, res) => res.render("home"));
+app.get("/cart", (_, res) => res.render("customers/cart"));
+app.get("/register", (_, res) => res.render("auth/register"));
+app.get("/login", (_, res) => res.render("auth/login"));
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
